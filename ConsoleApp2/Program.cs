78a9fdb91d1b24f006fa6fcc48dbe5a60e8861f5
aptem1223 <sanstop357@gmail.com>
2025-05-29 10:46:27 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Запрашиваем у пользователя данные для книги
+            Console.Write("Введите название книги: ");
+            string bookTitle = Console.ReadLine();
+
+            Console.Write("Введите имя автора: ");
+            string authorName = Console.ReadLine();
+
+            Console.Write("Введите содержание книги: ");
+            string bookContent = Console.ReadLine();
+
+            // Создаем объекты для Title, Author и Content
+            Title title = new Title(bookTitle);
+            Author author = new Author(authorName);
+            Content content = new Content(bookContent);
+
+            // Создаем объект Book с этими данными
+            Book book = new Book(title, author, content);
+
+            Console.WriteLine("\nИнформация о книге:\n");
+
+            // Выводим всю информацию цветными строками
+            book.Show();
+
+            // Ожидаем нажатия клавиши, чтобы окно не закрылось сразу
+            Console.ReadKey();
+        }
+    }
+}
